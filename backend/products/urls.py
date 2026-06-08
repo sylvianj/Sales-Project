6abf9256d06481d57ac0369efaa212f90ecf4ecc
Sylvia <125375898+sylvianj@users.noreply.github.com>
2026-06-08 09:25:19 +0300

    customers and products

---
+++ backend/products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from products.views import ProductViewSet
+
+router = DefaultRouter()
+router.register(r'', ProductViewSet, basename='product')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
